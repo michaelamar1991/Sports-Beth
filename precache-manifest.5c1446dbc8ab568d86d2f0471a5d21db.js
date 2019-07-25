ad3c6bbae2c1242b873155e9a0705ecda6642383
Miki <mik.amar91@gmail.com>
2019-07-25 12:28:58 +0300

    Updates

---
+++ precache-manifest.5c1446dbc8ab568d86d2f0471a5d21db.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0ece14b48264748e80fbd13479fce3",
+    "url": "/sports-beth/index.html"
+  },
+  {
+    "revision": "b3d79254f8375c3608f3",
+    "url": "/sports-beth/static/css/main.80c3d5e1.chunk.css"
+  },
+  {
+    "revision": "1e5ccd50c7608f0f72f4",
+    "url": "/sports-beth/static/js/2.1300203e.chunk.js"
+  },
+  {
+    "revision": "b3d79254f8375c3608f3",
+    "url": "/sports-beth/static/js/main.fcd17239.chunk.js"
+  },
+  {
+    "revision": "46e31ed256c54d5bc5aa",
+    "url": "/sports-beth/static/js/runtime~main.f803e560.js"
+  },
+  {
+    "revision": "4f9754fa3789d7c75c502aac82c184cc",
+    "url": "/sports-beth/static/media/Barcelona.4f9754fa.png"
+  },
+  {
+    "revision": "10e11d25f62f7cc2169406b468b12e26",
+    "url": "/sports-beth/static/media/Juventus.10e11d25.png"
+  },
+  {
+    "revision": "a6b0160469825d9c0c061b010dd7d59a",
+    "url": "/sports-beth/static/media/ManchesterUnited.a6b01604.png"
+  },
+  {
+    "revision": "70efcbab2446534e25a7cf7232b3ba90",
+    "url": "/sports-beth/static/media/RealMadrid.70efcbab.png"
+  },
+  {
+    "revision": "39a23a2128166d58c40a1975e69a6af8",
+    "url": "/sports-beth/static/media/cover.39a23a21.jpg"
+  }
+]);
